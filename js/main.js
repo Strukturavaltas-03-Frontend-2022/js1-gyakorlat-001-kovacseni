67c096216878f92301ed38c04feef5a4b632875c
kovacseni <kovacseni@gmail.com>
2023-05-03 16:35:37 +0200

    változók, függvények, tömbök

---
+++ js/main.js
@@ -0,0 +1,9 @@
+'use strict'
+
+const elements = document.querySelectorAll('.button');
+
+const handleClick = () => {
+    for (let i = 0; i < elements.length; i++) {
+        elements[i].addEventListener('click', () => console.log(elements[i].textContent));
+    }
+}
